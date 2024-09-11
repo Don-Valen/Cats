cc3747fdc2a9c09196d0b188853e1fe1c564a0ca
Valentina <dondokovavalya@mail.ru>
2024-09-11 17:56:34 +0800

    Добавлена кнопка для загрузки новых изображений

--- Cats.py
+++ Cats.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -15,6 +15,13 @@ def load_image():
         return None
 
 
+def set_image()
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
 window = Tk()
 window.title('Cats!')
 window.geometry('600x400')
@@ -22,11 +29,10 @@ window.geometry('600x400')
 label = Label()
 label.pack()
 
-url = 'https://catass.com/cat'
-img = load_image(url)
+update_button = Button(text='Обновить', command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = 'https://cataas.com/cat'
+set_image()
 
 window.mainloop()
